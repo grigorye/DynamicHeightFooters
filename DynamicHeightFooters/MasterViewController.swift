74b7e6c0a0d54f2a4d6381ef69efe2ce67cad86d
Grigorii Entin <grigorii.entin@booking.com>
2017-03-29 01:17:31 +0200

    Added more implementation options; segregated table view data source and delegate.

--- DynamicHeightFooters/MasterViewController.swift
+++ DynamicHeightFooters/MasterViewController.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class FooterView : UITableViewHeaderFooterView {
+class CustomFooterView : UITableViewHeaderFooterView {
+
+    var customTitleLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.numberOfLines = 0
+        $0.font = UIFont.systemFont(ofSize: 30)
+        return $0
+    } (UILabel())
     
-    var label = UILabel()
-    
-    func embedLabel() {
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.numberOfLines = 0
-        label.font = UIFont.systemFont(ofSize: 30)
-        #if false
-        let contentView = self
-        #endif
-        contentView.addSubview(label)
+    func prepareForUse() {
+        textLabel!.isHidden = true
+        contentView.addSubview(customTitleLabel)
         NSLayoutConstraint.activate({
             let attributes: [NSLayoutAttribute] = [
                 .trailingMargin,
@@ -28,64 +28,137 @@ class FooterView : UITableViewHeaderFooterView {
                 .bottomMargin
             ]
             return attributes.map {
-                NSLayoutConstraint(item: label, attribute: $0, relatedBy: .equal, toItem: contentView, attribute: $0, multiplier: 1, constant: 0)
+                NSLayoutConstraint(item: customTitleLabel, attribute: $0, relatedBy: .equal, toItem: contentView, attribute: $0, multiplier: 1, constant: 0)
             }
         }())
     }
     
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
-        embedLabel()
+        prepareForUse()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        embedLabel()
+        prepareForUse()
     }
+    
 }
 
-class MasterViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.register(FooterView.self, forHeaderFooterViewReuseIdentifier: "Footer")
-        #if false
-        tableView.sectionFooterHeight = UITableViewAutomaticDimension
-        tableView.estimatedSectionFooterHeight = 30
-        #endif
-    }
-
+class TableViewDataSource : NSObject, UITableViewDataSource {
+    
     var numberOfSections: Int = 100
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return numberOfSections
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
+        
         cell.textLabel!.text = "Section \(indexPath.section)"
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        let footerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "Footer")! as! FooterView
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
         let text = ((0..<section).map {"\($0)"}).joined(separator: "\n")
-        footerView.label.text = text
+        return text
+    }
+}
+
+class DynamicCustomFooterTableViewDelegate : NSObject, UITableViewDelegate {
+    
+    func prepare(_ tableView: UITableView) {
+        tableView.register(CustomFooterView.self, forHeaderFooterViewReuseIdentifier: "Footer")
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "Footer")! as! CustomFooterView
+        let title = tableView.dataSource?.tableView?(tableView, titleForFooterInSection: section)
+        footerView.customTitleLabel.text = title
         return footerView
     }
-
-    #if true
-    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return UITableViewAutomaticDimension
     }
-    override func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
         return 30
     }
-    #endif
+    
+}
+
+class DynamicStandardFooterTableViewDelegate : NSObject, UITableViewDelegate {
+    
+    func prepare(_ tableView: UITableView) {
+        tableView.register(UITableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: "Footer")
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "Footer")
+        return footerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
+        return 30
+    }
+    
+}
+
+class MasterViewController: UITableViewController {
+
+    enum DelegateKind {
+        enum DynamicSubKind {
+            case custom
+            case standard
+        }
+
+        case none
+        case viewController
+        case dynamic(subkind: DynamicSubKind)
+    }
+    
+    static let delegateKind: DelegateKind = .dynamic(subkind: .custom)
+
+    lazy var tableViewDelegate: UITableViewDelegate? = {
+        switch delegateKind {
+        case .none:
+            return nil
+        case .viewController:
+            return self
+        case .dynamic(let dynamicSubkind):
+            let tableView = self.tableView!
+            switch dynamicSubkind {
+            case .custom:
+                return {
+                    $0.prepare(tableView)
+                    return $0
+                }(DynamicCustomFooterTableViewDelegate())
+            case .standard:
+                return {
+                    $0.prepare(tableView)
+                    return $0
+                }(DynamicStandardFooterTableViewDelegate())
+            }
+        }
+    }()
+
+    let tableViewDataSource = TableViewDataSource()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = tableViewDataSource
+        tableView.delegate = tableViewDelegate
+        tableView.reloadData()
+    }
     
 }
